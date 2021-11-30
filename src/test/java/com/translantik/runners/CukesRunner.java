35f7bed0357219fe37b1fa0ea672066e105340e2
ibrahimkuralozkan <ibrahimkuralozkan@gmail.com>
2021-11-30 01:55:15 +0100

    ibrahim_Filter_Menu_US009.feature added

--- src/test/java/com/translantik/runners/CukesRunner.java
+++ src/test/java/com/translantik/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/translantik/stepdefinitions",
         dryRun = false,
-        tags = "@wip"
+        tags = "@login"
 )
 public class CukesRunner {
 }
