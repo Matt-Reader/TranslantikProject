494263ead7d1ab1849ce1ace7323f77b33cc4947
Nawidqayumi <qayumi@yandex.com>
2021-11-30 17:22:22 +0100

    vehicleStepDef added and feature to the vehicle page added

---
+++ src/test/java/com/translantik/stepdefinitions/vehicleStepDef.java
@@ -0,0 +1,21 @@
+package com.translantik.stepdefinitions;
+
+import com.translantik.pages.DashboardPage;
+import com.translantik.pages.LoginPage;
+import com.translantik.utilities.ConfigurationReader;
+import com.translantik.utilities.Driver;
+import io.cucumber.java.en.Given;
+
+public class vehicleStepDef {
+
+    LoginPage loginPage = new LoginPage();
+    DashboardPage dashboardPage = new DashboardPage();
+
+    @Given("the {string} is on the Vehicles page")
+    public void theIsOnTheVehiclesPage(String userType) {
+
+        Driver.get().get(ConfigurationReader.get("url"));
+        loginPage.LoginAsUserTypes(userType);
+        dashboardPage.navigateToModule("Fleet", "Vehicles");
+        dashboardPage.waitUntilLoaderScreenDisappear();
+}
