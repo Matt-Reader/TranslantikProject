8d32bbc183109fb2037e7446bdf3e650584519e9
KubilayG <kubilaygunerhan@hotmail.com>
2021-12-03 22:59:21 +0100

    Kubilay added US-001-Login Functionality

--- src/test/java/com/translantik/runners/CukesRunner.java
+++ src/test/java/com/translantik/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/translantik/stepdefinitions",
         dryRun = false,
-        tags = "@smoke"
+        tags = "@login"
 
 )
 public class CukesRunner {
